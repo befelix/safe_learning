6846d916856d34cd999fc1f3a21105624ea9e51a
Felix <befelix@ethz.ch>
2016-03-08 12:33:18 +0100

    pep8 and added GP sample function

--- utilities.py
+++ utilities.py
@@ -1,11 +1,15 @@
 from __future__ import division
+
+from collections import Sequence
+
 import numpy as np
 import scipy as sp
 
 
-__all__ = ['combinations', 'line_search_bisection', 'compute_v_dot',
-           'get_safe_set', 'find_max_levelset', 'lqr',
-           'quadratic_lyapunov_function']
+__all__ = ['combinations', 'linearly_spaced_combinations',
+           'line_search_bisection', 'compute_v_dot', 'get_safe_set',
+           'find_max_levelset', 'lqr', 'quadratic_lyapunov_function',
+           'sample_gp_function']
 
 
 def combinations(arrays):
@@ -13,7 +17,7 @@ def combinations(arrays):
 
     Parameters
     ----------
-    arrays - list of np.array
+    arrays: list of np.array
 
     Returns
     -------
@@ -21,6 +25,38 @@ def combinations(arrays):
         An array that contains all combinations of the input arrays
     """
     return np.array(np.meshgrid(*arrays)).T.reshape(-1, len(arrays))
+
+
+def linearly_spaced_combinations(bounds, num_samples):
+    """
+    Return 2-D array with all linearly spaced combinations with the bounds.
+
+    Parameters
+    ----------
+    bounds: sequence of tuples
+        The bounds for the variables, [(x1_min, x1_max), (x2_min, x2_max), ...]
+    num_samples: integer or array_likem
+        Number of samples to use for every dimension. Can be a constant if
+        the same number should be used for all, or an array to fine-tune
+        precision. Total number of data points is num_samples ** len(bounds).
+
+    Returns
+    -------
+    combinations: 2-d array
+        A 2-d arrray. If d = len(bounds) and l = prod(num_samples) then it
+        is of size l x d, that is, every row contains one combination of
+        inputs.
+    """
+    num_vars = len(bounds)
+    if not isinstance(num_samples, Sequence):
+        num_samples = [num_samples] * num_vars
+
+    # Create linearly spaced test inputs
+    inputs = [np.linspace(b[0], b[1], n) for b, n in zip(bounds,
+                                                         num_samples)]
+
+    # Convert to 2-D array
+    return combinations(inputs)
 
 
 def line_search_bisection(f, bound, accuracy):
@@ -211,4 +247,81 @@ def quadratic_lyapunov_function(x, P):
     dV - np.array
         2d array with dV(x)/dx on each row
     """
-    return np.sum(x.dot(P) * x, axis=1), 2 * x.dot(P) 
+    return np.sum(x.dot(P) * x, axis=1), 2 * x.dot(P)
+
+
+def sample_gp_function(kernel, bounds, num_samples, noise_var,
+                       interpolation='linear'):
+    """
+    Sample a function from a gp with corresponding kernel within its bounds.
+
+    Parameters
+    ----------
+    kernel: instance of GPy.kern.*
+    bounds: list of tuples
+        [(x1_min, x1_max), (x2_min, x2_max), ...]
+    num_samples: int or list
+        If integer draws the corresponding number of samples in all
+        dimensions and test all possible input combinations. If a list then
+        the list entries correspond to the number of linearly spaced samples of
+        the corresponding input
+    noise_var: float
+        Variance of the observation noise of the GP function
+    interpolation: string
+        If 'linear' interpolate linearly between samples, if 'kernel' use the
+        corresponding mean RKHS-function of the GP.
+
+    Returns
+    -------
+    function: object
+        function(x, noise=True)
+        A function that takes as inputs new locations x to be evaluated and
+        returns the corresponding noisy function values. If noise=False is
+        set the true function values are returned (useful for plotting).
+    """
+    inputs = linearly_spaced_combinations(bounds, num_samples)
+    cov = kernel.K(inputs) + np.eye(inputs.shape[0]) * 1e-6
+    output = np.random.multivariate_normal(np.zeros(inputs.shape[0]),
+                                           cov)
+
+    if interpolation == 'linear':
+
+        def evaluate_gp_function_linear(x, noise=True):
+            """
+            Evaluate the GP sample function with linear interpolation.
+
+            Parameters
+            ----------
+            x: np.array
+                2D array with inputs
+            noise: bool
+                Whether to include prediction noise
+            """
+            x = np.atleast_2d(x)
+            y = sp.interpolate.griddata(inputs, output, x, method='linear')
+            y = np.atleast_2d(y)
+            if noise:
+                y += np.sqrt(noise_var) * np.random.randn(x.shape[0], 1)
+            return y
+        return evaluate_gp_function_linear
+    elif interpolation == 'kernel':
+        cho_factor = sp.linalg.cho_factor(cov)
+        alpha = sp.linalg.cho_solve(cho_factor, output)
+
+        def evaluate_gp_function_kernel(x, noise=True):
+            """
+            Evaluate the GP sample function with kernel interpolation.
+
+            Parameters
+            ----------
+            x: np.array
+                2D array with inputs
+            noise: bool
+                Whether to include prediction noise
+            """
+            x = np.atleast_2d(x)
+            y = kernel.K(x, inputs).dot(alpha)
+            if noise:
+                y += np.sqrt(noise_var) * np.random.randn(x.shape[0], 1)
+            return y
+        return evaluate_gp_function_kernel
