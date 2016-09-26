766d314b94562a74f0c38c5a727b890e515d477c
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-09-26 13:57:24 +0200

    Add simple test

--- safe_rl/test.py
+++ safe_rl/test.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function, absolute_import
 
 from numpy.testing import *
+import unittest
 import numpy as np
 
 from .triangulation import Triangulation
@@ -9,7 +10,29 @@ from .triangulation import Triangulation
 class TriangulationTest(TestCase):
     """Test the Triangulization method"""
 
-    def test_initilization(self):
 
-        points = np.random.randn((10, 2))
-        Triangulation(points)
+    def test_values(self):
+        points = np.array([[0, 0],
+                           [1, 0],
+                           [0, 1]])
+
+        tri = Triangulation(points)
+
+        test_points = np.vstack((points, np.array([[0.5, 0.5],
+                                                   [0, 0.5],
+                                                   [0.5, 0]])))
+
+        H = tri.function_values_at(test_points).todense()
+
+        true_H = np.array([[1, 0, 0],
+                           [0, 1, 0],
+                           [0, 0, 1],
+                           [0, 0.5, 0.5],
+                           [0.5, 0, 0.5],
+                           [0.5, 0.5, 0]])
+
+        assert_allclose(H, true_H)
+
+
+if __name__ == '__main__':
+    unittest.main()
