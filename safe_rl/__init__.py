49b567e7e0bab507b9aa6b604cdc1d08dc83ab9f
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-09-26 13:23:05 +0200

    Add unit tests

--- safe_rl/__init__.py
+++ safe_rl/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import absolute_import
 
 from .triangulization import *
+
+# Import test after __all__ (no documentation)
+from numpy.testing import Tester
+test = Tester().test
+
+# Add everything to __all__
+__all__ = [s for s in dir() if not s.startswith('_')]
---
+++ safe_rl/test.py
@@ -0,0 +1,15 @@
+from __future__ import division, print_function, absolute_import
+
+from numpy.testing import *
+import numpy as np
+
+from .triangulization import Triangulation
+
+
+class TriangulationTest(TestCase):
+    """Test the Triangulization method"""
+
+    def test_initilization(self):
+
+        points = np.random.randn((10, 2))
+        Triangulation(points)
