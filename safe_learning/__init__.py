ed3fac953562faf554cfb2ecab4c16946e0cf33b
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-10-03 08:16:44 +0200

    Merge the two projects

--- safe_learning/__init__.py
+++ safe_learning/__init__.py
@@ -1,2 +1,12 @@
-from utilities import *
-from safe_learning import *
+from __future__ import absolute_import
+
+from .utilities import *
+from .safe_learning import *
+from .triangulation import *
+
+# Import test after __all__ (no documentation)
+from numpy.testing import Tester
+test = Tester().test
+
+# Add everything to __all__
+__all__ = [s for s in dir() if not s.startswith('_')]
