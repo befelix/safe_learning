159da0d814bae67dc2540bf1d6ce5dc00445a107
Felix Berkenkamp <fberkenkamp@gmail.com>
2017-03-14 15:58:17 +0100

    Add error to run_tests without pytest

--- safe_learning/__init__.py
+++ safe_learning/__init__.py
@@ -9,7 +9,7 @@ try:
     from pytest import main as run_tests
 except ImportError:
     def run_tests():
-        print('Testing requires the pytest package.')
+        raise ImportError('Testing requires the pytest package.')
 
 # Add everything to __all__
 __all__ = [s for s in dir() if not s.startswith('_')]
